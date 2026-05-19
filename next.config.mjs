87fe32aa4175e2559b9f68dddeffd434822c6d87
Сергей Рудаков <gadget2940@gmail.com>
2026-05-19 10:14:08 +0300

    remove basePath for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
